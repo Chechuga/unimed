7c9e45cfd174a5c46aa4766e12dfc414de661225
Chechuga <HammsterMail@gmail.com>
2022-01-06 21:51:01 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 5000
 const router = express.Router();
 
 
-router.route('/2_5271552321199804659').get((req, res) => { res.sendFile(__dirname + '/2_5271552321199804659.pdf')})
+router.get('/2_5271552321199804659', (req, res) => { res.sendFile(__dirname + '/2_5271552321199804659.pdf')})
 .listen(PORT, () => console.log('Listening on ${ PORT }'))
 
-router.route('/2_5271633072579108412').get((req, res) => { res.sendFile(__dirname + '/2_5271633072579108412.pdf')})
+router.get('/2_5271633072579108412', (req, res) => { res.sendFile(__dirname + '/2_5271633072579108412.pdf')})
 .listen(PORT, () => console.log('Listening on ${ PORT }'))
