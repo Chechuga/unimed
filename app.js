8ed8f0a31b5cc36c694bb3f1351b1f1bc94f529b
Chechuga <HammsterMail@gmail.com>
2022-01-06 22:07:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const router = express.Router();
+var router = express.Router();
 
 
 router.get('/2_5271552321199804659', (req, res) => { res.sendFile(__dirname + '/2_5271552321199804659.pdf')})
